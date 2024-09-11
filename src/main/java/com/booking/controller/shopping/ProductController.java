29183e1d164d1bfb45e54475b2d3826f32cd3a9d
agony1997 <a03046578@gmail.com>
2024-09-11 17:08:00 +0800

    范.修改:商城:修正各頁面manageListName，清除controller多餘程式碼。 (#20)

--- src/main/java/com/booking/controller/shopping/ProductController.java
+++ src/main/java/com/booking/controller/shopping/ProductController.java
@@ -7,7 +7,6 @@ import org.hibernate.Session;
 
 import com.booking.bean.shopping.Product;
 import com.booking.service.shopping.ProductService;
-import com.booking.utils.HibernateUtil;
 import com.booking.utils.Result;
 
 import jakarta.servlet.ServletException;
@@ -17,7 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = { "/product/create", "/product/delete", "/product/update", "/product/selectUpdate",
-		"/product/selectName", "/product/selectAll", "/product" })
+		"/product/selectName", "/product/selectAll", "/product","/product/sendCreate" })
 public class ProductController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -33,9 +32,11 @@ public class ProductController extends HttpServlet {
 		productService = new ProductService(session);
 		
 		response.setHeader("Access-Control-Allow-Origin", "*");
-
+		request.setAttribute("manageListName", "商城列表");
+		
 		switch (path) {
 		case "create" -> create(request, response);
+		case "sendCreate" -> sendCreate(request, response);
 		case "delete" -> delete(request, response);
 		case "update" -> update(request, response);
 		case "selectUpdate" -> selectUpdate(request, response);
@@ -48,7 +49,7 @@ public class ProductController extends HttpServlet {
 
 	private void sendProductIndex(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setAttribute("manageListName", "商城列表");
+		
 		request.getRequestDispatcher("/adminsystem/shopping/product-select.jsp").forward(request, response);
 	}
 
@@ -61,7 +62,6 @@ public class ProductController extends HttpServlet {
 
 		Result<List<Product>> productResult = productService.getProductByName(productName, sortBy, sortOrder);
 		List<Product> productList = productResult.getData();
-
 		request.setAttribute("products", productList);
 		request.setAttribute("seleteName", productName);
 		request.getRequestDispatcher("/adminsystem/shopping/product-select.jsp").forward(request, response);
@@ -74,21 +74,16 @@ public class ProductController extends HttpServlet {
 		String sortBy = request.getParameter("sortBy");
 		String sortOrder = request.getParameter("sortOrder");
 
-		if (sortBy == null || sortBy.isEmpty()) {
-			sortBy = "productId"; // 默認排序字段
-		}
-		if (sortOrder == null || sortOrder.isEmpty()) {
-			sortOrder = "DESC"; // 默認排序方式
-		}
-
 		Result<List<Product>> productResult = productService.getAllProduct(sortBy, sortOrder);
 		List<Product> productList = productResult.getData();
-
 		request.setAttribute("products", productList);
 		request.getRequestDispatcher("/adminsystem/shopping/product-select.jsp").forward(request, response);
 
 	}
-
+	private void sendCreate(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		request.getRequestDispatcher("/adminsystem/shopping/product-create.jsp").forward(request, response);
+	}
 	private void create(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
 		Integer categoryId = Integer.parseInt(request.getParameter("category-id"));
@@ -98,7 +93,7 @@ public class ProductController extends HttpServlet {
 		Integer productSales = Integer.parseInt(request.getParameter("product-sales"));
 		Integer productInventorey = Integer.parseInt(request.getParameter("product-inventorey"));
 		Integer productState = Integer.parseInt(request.getParameter("product-state"));
-
+		// 缺圖片
 		Product product = new Product(categoryId, productName, null, productDescription, productPrice, productSales,
 				productInventorey, productState);
 
@@ -106,23 +101,6 @@ public class ProductController extends HttpServlet {
 		response.sendRedirect(request.getContextPath() + "/product/selectAll");
 	}
 
-	class CreateResponse {
-		private boolean success;
-		private Object data;
-
-		public CreateResponse(boolean success, Object data) {
-			this.success = success;
-			this.data = data;
-		}
-
-		public boolean isSuccess() {
-			return success;
-		}
-
-		public Object getData() {
-			return data;
-		}
-	}
 
 	private void delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -130,27 +108,20 @@ public class ProductController extends HttpServlet {
 		Result<Product> result = productService.getProductById(productId);
 		Product product = result.getData();
 		productService.removeProduct(productId);
-
+		
 		request.setAttribute("product", product);
 		request.setAttribute("deleteId", productId);
 		response.sendRedirect(request.getContextPath() + "/product/selectAll");
 
 	}
 
-	/**
-	 * 選取要更新的那筆
-	 * 
-	 * @param request
-	 * @param response
-	 * @throws IOException
-	 * @throws ServletException
-	 */
 	private void selectUpdate(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 		
 		Integer productId = Integer.parseInt(request.getParameter("product-id"));
 		Result<Product> result = productService.getProductById(productId);
 		Product product = result.getData();
+
 		request.setAttribute("product", product);
 		request.setAttribute("updateId", productId);
 		request.getRequestDispatcher("/adminsystem/shopping/product-update.jsp").forward(request, response);
@@ -161,18 +132,18 @@ public class ProductController extends HttpServlet {
 		Integer productId = Integer.parseInt(request.getParameter("product-id"));
 		Integer categoryId = Integer.parseInt(request.getParameter("category-id"));
 		String productName = request.getParameter("product-name");
-//		String productImage = request.getParameter("product-image");
 		String productDescription = request.getParameter("product-description");
 		Integer productPrice = Integer.parseInt(request.getParameter("product-price"));
 		Integer productSales = Integer.parseInt(request.getParameter("product-sales"));
 		Integer productInventorey = Integer.parseInt(request.getParameter("product-inventorey"));
 		Integer productState = Integer.parseInt(request.getParameter("product-state"));
-
+		// 缺圖片
 		Product product = new Product(productId, categoryId, productName, null, productDescription, productPrice,
 				productSales, productInventorey, productState);
 
 		productService.updateProduct(product);
-
+		// 利用Referrer，從Update頁面轉回原頁面
+		// 沒防Open Redirect攻擊
 		String referrer = request.getParameter("referrer");
 		response.sendRedirect(referrer);
 
