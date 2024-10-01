b41a353d104788f9ad54b2ce06457a8a0fb182b1
agony1997 <a03046578@gmail.com>
2024-10-01 20:33:30 +0800

    范:新增 增加其他表格中 (#37)

---
+++ src/main/java/com/booking/dao/shopping/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.booking.dao.shopping;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.booking.bean.pojo.shopping.ProductCategory;
+
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+
+}
