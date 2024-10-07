c626451f5606a906638e68bd8500159c238db2f1
Makoto-Ha <s51r3040@gmail.com>
2024-10-07 10:20:54 +0800

    黃威誠.新增: 點擊房型後顯示該所有房間

--- src/main/webapp/adminsystem/attraction/form.js
+++
@@ -1,7 +0,0 @@
-const selectCity = document.getElementById("select-city");
-
-data.forEach(location => {
-	let optionCity = document.createElement("option");
-	optionCity.textContent = location.name;
-	selectCity.appendChild(optionCity);
-});
--- src/main/webapp/adminsystem/shopping/back.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.cancel').addEventListener('click', function() {
-	history.back();
-});
