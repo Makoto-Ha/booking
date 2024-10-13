a15448de3d71837ce3a585f8e119a95799ccc3ad
Makoto-Ha <s51r3040@gmail.com>
2024-10-13 20:25:40 +0800

    黃威誠.新增: room-detail前端的房間資訊頁面

--- src/main/java/com/booking/controller/common/UserController.java
+++ src/main/java/com/booking/controller/common/UserController.java
@@ -2,6 +2,8 @@ package com.booking.controller.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class UserController {
@@ -14,4 +16,10 @@ public class UserController {
 	private String sendUserSearch() {
 		return "/frontend/booking/user-search";
 	}
+	
+	@GetMapping("/room/detail")
+	public String sendRoomDetail() {
+		return "/frontend/booking/room-detail";
+	}
+	
 }
