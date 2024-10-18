7a06939cecd0b67fe26fedc3df68e47522be4756
huang-chenwei <tony105324047@gmail.com>
2024-10-18 17:48:55 +0800

    黃振瑋.修改:編輯使用者頁面

--- src/main/java/com/booking/bean/pojo/user/User.java
+++ src/main/java/com/booking/bean/pojo/user/User.java
@@ -1,13 +1,16 @@
 package com.booking.bean.pojo.user;
 
-import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
-import com.booking.bean.pojo.attraction.PackageTour;
-import com.booking.bean.pojo.booking.BookingOrder;
-import com.booking.bean.pojo.shopping.ShoppingOrder;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
