e397163f182173f7940430b4f7c0c618a4b99b88
Makoto-Ha <s51r3040@gmail.com>
2024-10-19 18:38:08 +0800

    黃威誠.新增: 用戶端房型列表支援簡易搜尋

--- src/main/java/com/booking/controller/booking/client/BookingClientJsonHandler.java
+++ src/main/java/com/booking/controller/booking/client/BookingClientJsonHandler.java
@@ -3,12 +3,14 @@ package com.booking.controller.booking.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.booking.bean.dto.booking.RoomtypeDTO;
+import com.booking.bean.dto.booking.client.RoomtypeKeywordSearchDTO;
 import com.booking.bean.dto.booking.client.RoomtypeSearchDTO;
 import com.booking.service.booking.client.RoomtypeClientService;
 
@@ -23,6 +25,12 @@ public class BookingClientJsonHandler {
 	private ResponseEntity<?> searchRoomtypes(@RequestBody RoomtypeSearchDTO roomtypeSearchDTO) {
 		System.out.println(roomtypeSearchDTO);
 		Page<RoomtypeDTO> page = rtClientService.searchRoomtypes(roomtypeSearchDTO);
+		return ResponseEntity.ok(page);
+	}
+	
+	@GetMapping("/roomtype")
+	private ResponseEntity<?> searchRoomtypesByKeywords(RoomtypeKeywordSearchDTO roomtypeKeywordSearchDTO) {
+		Page<RoomtypeDTO> page = rtClientService.searchRoomtypesByKeyword(roomtypeKeywordSearchDTO);
 		return ResponseEntity.ok(page);
 	}
 	
