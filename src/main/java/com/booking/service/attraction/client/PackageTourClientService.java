f72191ba2838d0e1d35d164cd7d24c18f51eb622
rting1485 <rting1485@gmail.com>
2024-10-24 11:12:17 +0800

    鄭家霖.新增:新增套裝行程詳細資訊頁面

--- src/main/java/com/booking/service/attraction/client/PackageTourClientService.java
+++ src/main/java/com/booking/service/attraction/client/PackageTourClientService.java
@@ -7,9 +7,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.booking.bean.dto.attraction.AttractionDTO;
 import com.booking.bean.dto.attraction.PackageTourDTO;
+import com.booking.bean.pojo.attraction.Attraction;
 import com.booking.bean.pojo.attraction.PackageTour;
 import com.booking.bean.pojo.attraction.PackageTourAttraction;
+import com.booking.dao.attraction.AttractionRepository;
 import com.booking.dao.attraction.PackageTourRepository;
 
 @Service
@@ -18,6 +21,9 @@ public class PackageTourClientService {
     @Autowired
     private PackageTourRepository packageTourRepo;
 
+    
+    @Autowired
+    private AttractionRepository attractionRepo;
     
     /**
      * 根據關鍵字搜尋套裝行程
@@ -65,6 +71,26 @@ public class PackageTourClientService {
         packageTourDTO.setAttractionNames(attractionNames);
 
         return packageTourDTO;
+    }
+    
+    
+    public List<AttractionDTO> getAttractionsByName(List<String> attractionNames) {
+        if (attractionNames == null || attractionNames.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        List<AttractionDTO> attractionDTOs = new ArrayList<>();
+        
+        for (String name : attractionNames) {
+            List<Attraction> attractions = attractionRepo.findAttractions(name, null, null, 0, 1);
+            if (!attractions.isEmpty()) {
+                AttractionDTO attractionDTO = new AttractionDTO();
+                BeanUtils.copyProperties(attractions.get(0), attractionDTO);
+                attractionDTOs.add(attractionDTO);
+            }
+        }
+        
+        return attractionDTOs;
     }
 }
 
